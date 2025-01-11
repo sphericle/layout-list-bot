6c99da8d3d1f4f479182471ed5fa0baa1457e28a
sphericle <86196223+sphericle@users.noreply.github.com>
2025-01-11 10:58:48 -0500

    d

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
+
+git pull
+forever restartall
