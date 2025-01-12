76678debd863c36c58a6f1e708ceea7f1b463e4c
sphericle <86196223+sphericle@users.noreply.github.com>
2025-01-12 09:42:05 -0500

    tests

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
 const { Octokit } = require("@octokit/rest");
 const { createDbSchema, createCacheDbSchema } = require("./others/dbSchema.js");
 const { parseUsers } = require("./others/gitUtils.js");
+const { test } = require("./test.js");
 const {
     clientInit,
     sequelizeInit,
@@ -80,6 +81,7 @@ async function start() {
         process.exit(1);
     }
     await checkGithubPermissions(octokit);
+    
     try {
         logger.info("Logging in client with discord...");
         await client.login(process.env.TOKEN);
