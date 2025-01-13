4d524b576c7664686f3792b74ad50dd8d7274b7e
sphericle <86196223+sphericle@users.noreply.github.com>
2025-01-12 19:58:38 -0500

    i hate this officially

--- commands/others/gif.js
+++ commands/others/gif.js
@@ -14,7 +14,7 @@ module.exports = {
         await interaction.deferReply();
         const { db } = require("../../index.js");
         // find the stored nextVal in the db
-        const nextVal = await db.nextGif.findOne({ where: {} });
+        let nextVal = await db.nextGif.findOne({ where: {} });
         const search = interaction.options.getString("search") || null;
         let url = "https://g.tenor.com/v1/search?key=LIVDSRZULELA&limit=20&contentfilter=medium";
         if (search) {
@@ -26,23 +26,61 @@ module.exports = {
         };
         const req = await fetch(url);
         const result = await req.json();
-        logger.log(result);
-        const randomIndex = Math.floor(Math.random() * result.results.length);
-        // pick a random gif from the response and send the url
-        const gif = result.results[randomIndex];
-        logger.log(gif)
-        await interaction.editReply(
-            gif.content_description ? 
-            `-# [${gif.content_description}](${gif.url})` :
-            gif.url
-        );
-        // randomly update the nextVal offset for the tenor api
-        // https://tenor.com/gifapi/documentation#endpoints-search
-        if (randomIndex % 3 !== 0)
+        if (result.results.length > 0) {
+            const randomIndex = Math.floor(Math.random() * result.results.length);
+            // pick a random gif from the response and send the url
+            const gif = result.results[randomIndex];
+            logger.log(gif)
+            await interaction.editReply(
+                gif.content_description ? 
+                `-# [${gif.content_description}](${gif.url})` :
+                gif.url
+            );
+            
+            // randomly update the nextVal offset for the tenor api
+            // https://tenor.com/gifapi/documentation#endpoints-search
+            if (randomIndex % 3 !== 0)
+                await db.nextGif.update(
+                    { value: result.next },
+                    { where: {} },
+                )
+            return;
+        } else {
             await db.nextGif.update(
-                { value: result.next },
+                { value: null },
                 { where: {} },
             )
-        return;
+            nextVal.value = null
+            // redo literally everything (yikes)
+            const search = interaction.options.getString("search") || null;
+            let url = "https://g.tenor.com/v1/search?key=LIVDSRZULELA&limit=20&contentfilter=medium";
+            if (search) {
+                const encodedSearch = encodeURIComponent(search); // filter unusable characters
+                url += `&q=${encodedSearch}`;
+            };
+            if (nextVal && nextVal.value) {
+                url += `&pos=${nextVal.value}`;
+            };
+            const req = await fetch(url);
+            const result = await req.json();
+            const randomIndex = Math.floor(Math.random() * result.results.length);
+            // pick a random gif from the response and send the url
+            const gif = result.results[randomIndex];
+            logger.log(gif)
+            await interaction.editReply(
+                gif.content_description ? 
+                `-# [${gif.content_description}](${gif.url})` :
+                gif.url
+            );
+            
+            // randomly update the nextVal offset for the tenor api
+            // https://tenor.com/gifapi/documentation#endpoints-search
+            if (randomIndex % 3 !== 0)
+                await db.nextGif.update(
+                    { value: result.next },
+                    { where: {} },
+                )
+            return;
+        }
     },
 };
