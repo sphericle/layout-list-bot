cbb2ed6ea8472d89288e9098f62371e15f72ab26
sphericle <86196223+sphericle@users.noreply.github.com>
2025-01-25 14:16:59 -0500

    im not testing this

--- commands/reliable/reliable.js
+++ commands/reliable/reliable.js
@@ -81,107 +81,102 @@ module.exports = {
                 );
             }
 
-            await interaction.editReply("Checking thread name...");
-            // get the thread name
-            const text = await interaction.channel.name;
-            const matchLevelName = text.match(/^(.*)\s\d+-\d+$/);
-            const matchYes = text.match(/(\d+)-\d+$/);
-            const matchNo = text.match(/\d+-(\d+)$/);
-            let count = 0; // Initialize the yes count to an integer
-            if (matchYes) {
-                try {
-                    count = parseInt(matchYes[1]); // Parse the regex output and add 1 vote to it
-                    count += 1;
+            let dbEntry = await db.levelsInVoting.findOne({
+                where: { discordid: interaction.channel.id },
+            });
 
-                    await interaction.editReply({
-                        content:
-                            "Changing thread name, this could take a while...",
-                        flags: MessageFlags.Ephemeral,
-                    });
-
-                    // get last 10 messages in channel
-                    const messages = await interaction.channel.messages.fetch({
-                        limit: 10,
-                    });
-
-                    // find the most recent message that contains "vote: "
-                    const voteMessage = messages.find((msg) =>
-                        msg.content.toLowerCase().includes("vote:")
-                    );
-
-                    // pin the message
-                    if (voteMessage)
-                        await voteMessage.pin(
-                            `Vote added by ${interaction.user.username}`
-                        );
-
-                    const message = await interaction.channel.send(
-                        `The vote is now at **${count}-${matchNo[1]}**. The thread name is being updated!!`
-                    );
-
-                    await interaction.channel.setName(
-                        `${matchLevelName[1]} ${count}-${matchNo[1]}`
-                    ); // Set the channel name to the same thing but with the added yes
-
-                    await message.delete();
-
-                    // update entry in db
-                    await db.levelsInVoting.update(
-                        { yeses: count },
-                        { where: { discordid: await interaction.channel.id } }
-                    );
-
-                    const dbEntry = await db.levelsInVoting.findOne({
-                        where: { discordid: await interaction.channel.id },
-                    });
-
-                    if (dbEntry) {
-                        const entry = dbEntry.dataValues;
-
-                        let shared = entry.shared.split(";");
-                        shared.pop();
-
-                        for (const user of shared) {
-                            const submitterDb = await db.submitters.findOne({
-                                where: {
-                                    discordid: Sequelize.where(
-                                        Sequelize.fn(
-                                            "LOWER",
-                                            Sequelize.col("discordid")
-                                        ),
-                                        "LIKE",
-                                        "%" + user + "%"
-                                    ),
-                                },
-                            });
-
-                            // check if the user has dmFlag set to true
-                            if (submitterDb.dataValues.dmFlag) {
-                                // get user by id of entry.submitter
-                                const submitter =
-                                    await interaction.guild.members.fetch(
-                                        entry.submitter
-                                    );
-                                await submitter.send(
-                                    `The level _${matchLevelName[1]}_ has received a new yes vote!\nThe vote is now at **${count}-${matchNo[1]}**.\n-# _To disable these messages, use the \`/vote dm\` command._`
-                                );
-                            }
-                        }
-                    }
-                } catch (e) {
-                    logger.error(`Error: ${e}`);
-                    return await interaction.editReply(
-                        `Something went wrong: ${e}`
-                    );
-                }
-            } else {
+            if (dbEntry.paused) {
                 return await interaction.editReply(
-                    "This thread name is not formatted correctly! (Level name #-#)"
+                    "This thread is paused, you can't vote on it!"
+                )
+            }
+
+            await interaction.editReply("Checking thread name...");
+            try {
+
+                await interaction.editReply({
+                    content:
+                        "Changing thread name, this could take a while...",
+                    flags: MessageFlags.Ephemeral,
+                });
+
+                // get last 10 messages in channel
+                const messages = await interaction.channel.messages.fetch({
+                    limit: 10,
+                });
+
+                // find the most recent message that contains "vote: "
+                const voteMessage = messages.find((msg) =>
+                    msg.content.toLowerCase().includes("vote:")
+                );
+
+                // pin the message
+                if (voteMessage)
+                    await voteMessage.pin(
+                        `Vote added by ${interaction.user.username}`
+                    );
+
+                const message = await interaction.channel.send(
+                    `The vote is now at **${dbEntry.yeses + 1}-${dbEntry.nos}**. The thread name is being updated!!`
+                );
+
+                await interaction.channel.setName(
+                    `${dbEntry.levelname} ${dbEntry.yeses + 1}-${dbEntry.nos}`
+                ); // Set the channel name to the same thing but with the added yes
+
+                await message.delete();
+
+                // update entry in db
+                await db.levelsInVoting.update(
+                    { yeses: dbEntry.yeses + 1 },
+                    { where: { discordid: await interaction.channel.id } }
+                );
+                    
+                dbEntry = await db.levelsInVoting.findOne({
+                    where: { discordid: await interaction.channel.id },
+                });
+
+                const entry = dbEntry.dataValues;
+
+                let shared = entry.shared.split(";");
+                shared.pop();
+
+                for (const user of shared) {
+                    const submitterDb = await db.submitters.findOne({
+                        where: {
+                            discordid: Sequelize.where(
+                                Sequelize.fn(
+                                    "LOWER",
+                                    Sequelize.col("discordid")
+                                ),
+                                "LIKE",
+                                "%" + user + "%"
+                            ),
+                        },
+                    });
+
+                    // check if the user has dmFlag set to true
+                    if (submitterDb.dataValues.dmFlag) {
+                        // get user by id of entry.submitter
+                        const submitter =
+                            await interaction.guild.members.fetch(
+                                entry.submitter
+                            );
+                        await submitter.send(
+                            `The level _${dbEntry.levelname}_ has received a new yes vote!\nThe vote is now at **${dbEntry.yeses}-${dbEntry.nos}**.\n-# _To disable these messages, use the \`/vote dm\` command._`
+                        );
+                    }
+                }
+                
+            } catch (e) {
+                logger.error(`Error: ${e}`);
+                return await interaction.editReply(
+                    `Something went wrong: ${e}`
                 );
             }
 
-            // ping user if needed
             return await interaction.editReply("Updated!");
+
         } else if (interaction.options.getSubcommand() === "no") {
             const { db } = require("../../index.js");
             await interaction.editReply("Checking thread name...");
@@ -192,98 +187,92 @@ module.exports = {
                 );
             }
 
-            // get the thread name
-            const text = interaction.channel.name;
-            const matchLevelName = text.match(/^(.*)\s\d+-\d+$/);
-            const matchYes = text.match(/(\d+)-\d+$/);
-            const matchNo = text.match(/\d+-(\d+)$/);
-            let count = 0; // Initialize the yes count to an integer
-            if (matchNo) {
-                try {
-                    count = parseInt(matchNo[1]); // Parse the regex output and add 1 vote to it
-                    count += 1;
+            let dbEntry = await db.levelsInVoting.findOne({
+                where: { discordid: interaction.channel.id },
+            });
 
-                    await interaction.editReply({
-                        content:
-                            "Changing thread name, this could take a while...",
-                        flags: MessageFlags.Ephemeral,
-                    });
-
-                    // get last 10 messages in channel
-                    const messages = await interaction.channel.messages.fetch({
-                        limit: 10,
-                    });
-
-                    // find the most recent message that contains "vote: "
-                    const voteMessage = messages.find((msg) =>
-                        msg.content.toLowerCase().includes("vote:")
-                    );
-
-                    // pin the message
-                    if (voteMessage) await voteMessage.pin();
-
-                    const message = await interaction.channel.send(
-                        `The vote is now at **${matchYes[1]}-${count}**.`
-                    );
-
-                    await interaction.channel.setName(
-                        `${matchLevelName[1]} ${matchYes[1]}-${count}`
-                    ); // Set the channel name to the same thing but with the added no
-
-                    await message.delete();
-
-                    // update entry in db
-                    await db.levelsInVoting.update(
-                        { nos: count },
-                        { where: { discordid: await interaction.channel.id } }
-                    );
-
-                    const dbEntry = await db.levelsInVoting.findOne({
-                        where: { discordid: await interaction.channel.id },
-                    });
-
-                    if (dbEntry) {
-                        const entry = dbEntry.dataValues;
-
-                        let shared = entry.shared.split(";");
-                        shared.pop();
-
-                        for (const user of shared) {
-                            const submitterDb = await db.submitters.findOne({
-                                where: {
-                                    discordid: Sequelize.where(
-                                        Sequelize.fn(
-                                            "LOWER",
-                                            Sequelize.col("discordid")
-                                        ),
-                                        "LIKE",
-                                        "%" + user + "%"
-                                    ),
-                                },
-                            });
-
-                            // check if the user has dmFlag set to true
-                            if (submitterDb.dataValues.dmFlag) {
-                                // get user by id of entry.submitter
-                                const submitter =
-                                    await interaction.guild.members.fetch(
-                                        entry.submitter
-                                    );
-                                await submitter.send(
-                                    `The level _${matchLevelName[1]}_ has received a no vote...\nThe vote is now at **${matchYes[1]}-${count}**.\n-# _To disable these messages, use the \`/vote dm\` command._`
-                                );
-                            }
-                        }
-                    }
-                } catch (e) {
-                    logger.error(`Error: ${e}`);
-                    return await interaction.editReply(
-                        `Something went wrong: ${e}`
-                    );
-                }
-            } else {
+            if (dbEntry.paused) {
                 return await interaction.editReply(
-                    "This thread name is not formatted correctly! (Level name #-#)"
+                    "This thread is paused, you can't vote on it!"
+                )
+            }
+            
+            try {
+
+                await interaction.editReply({
+                    content:
+                        "Changing thread name, this could take a while...",
+                    flags: MessageFlags.Ephemeral,
+                });
+
+                // get last 10 messages in channel
+                const messages = await interaction.channel.messages.fetch({
+                    limit: 10,
+                });
+
+                // find the most recent message that contains "vote: "
+                const voteMessage = messages.find((msg) =>
+                    msg.content.toLowerCase().includes("vote:")
+                );
+
+                // pin the message
+                if (voteMessage) await voteMessage.pin();
+
+                const message = await interaction.channel.send(
+                    `The vote is now at **${dbEntry.yeses}-${dbEntry.nos + 1}**.`
+                );
+
+                await interaction.channel.setName(
+                    `${dbEntry.levelname} ${dbEntry.yeses}-${dbEntry.nos + 1}`
+                ); // Set the channel name to the same thing but with the added no
+
+                await message.delete();
+
+                // update entry in db
+                await db.levelsInVoting.update(
+                    { nos: dbEntry.nos + 1 },
+                    { where: { discordid: await interaction.channel.id } }
+                );
+
+                dbEntry = await db.levelsInVoting.findOne({
+                    where: { discordid: await interaction.channel.id },
+                });
+
+                const entry = dbEntry.dataValues;
+
+                let shared = entry.shared.split(";");
+                shared.pop();
+
+                for (const user of shared) {
+                    const submitterDb = await db.submitters.findOne({
+                        where: {
+                            discordid: Sequelize.where(
+                                Sequelize.fn(
+                                    "LOWER",
+                                    Sequelize.col("discordid")
+                                ),
+                                "LIKE",
+                                "%" + user + "%"
+                            ),
+                        },
+                    });
+
+                    // check if the user has dmFlag set to true
+                    if (submitterDb.dataValues.dmFlag) {
+                        // get user by id of entry.submitter
+                        const submitter =
+                            await interaction.guild.members.fetch(
+                                entry.submitter
+                            );
+                        await submitter.send(
+                            `The level _${dbEntry.levelname}_ has received a no vote...\nThe vote is now at **${dbEntry.yeses}-${dbEntry.nos}**.\n-# _To disable these messages, use the \`/vote dm\` command._`
+                        );
+                    }
+                }
+            } catch (e) {
+                logger.error(`Error: ${e}`);
+                return await interaction.editReply(
+                    `Something went wrong: ${e}`
                 );
             }
 
@@ -312,21 +301,12 @@ module.exports = {
                 );
             }
             await interaction.editReply("Fetching thread info...");
-            // get the thread name
-            const text = interaction.channel.name;
-            const matchLevelName = text.match(/^(.*)\s\d+-\d+$/);
-            if (!matchLevelName) {
-                return await interaction.editReply(
-                    "This thread's name is not formatted correctly! (Level name #-#)"
-                );
-            }
-            const levelName = matchLevelName[1];
 
             const embed = new EmbedBuilder()
                 .setTitle(
                     command === "accept"
-                        ? `Accepted: ${levelName}`
-                        : `Rejected: ${levelName}`
+                        ? `Accepted: ${submission.levelname}`
+                        : `Rejected: ${submission.levelname}`
                 )
                 .setColor(command === "accept" ? 0x00ff00 : 0xff0000)
                 .setTimestamp();
@@ -338,7 +318,6 @@ module.exports = {
             await interaction.editReply("Sending message...");
             const shared = submission.shared.split(";");
             shared.pop();
-            logger.log(shared);
             let pingMessage = "";
             for (const user of shared) pingMessage += `<@${user}> `;
 
@@ -374,14 +353,14 @@ module.exports = {
             );
 
             await interaction.channel.setName(
-                `${matchLevelName[1]} (${
+                `${submission.levelname} (${
                     command === "accept" ? "ACCEPTED" : "REJECTED"
                 })`
             ); // Set the channel name to the same thing but with the added yes
 
             await message.delete();
 
-            // Create button to remove the message
+            // Create button to delete the thread
             const deleteThread = new ButtonBuilder()
                 .setCustomId("deleteThread")
                 .setLabel("Delete Thread")
