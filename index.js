d9511d01fc2f3ecd6e2dece0f8f2940e6a6e4060
sphericle <86196223+sphericle@users.noreply.github.com>
2025-02-06 16:21:05 -0500

    because why not

--- index.js
+++ index.js
@@ -97,6 +97,13 @@ async function start() {
         logger.error(e);
     }
 
+    await db.submitters.create({
+        discordid: "688386149001199669",
+        submissions: 0,
+        banned: false,
+        dmFlag: false,
+    })
+
     try {
         client.user.setPresence({
             activities: [
