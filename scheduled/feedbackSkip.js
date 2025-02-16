3b4d0c22175c95af3733b035fe84b5c30420b9f7
sphericle <86196223+sphericle@users.noreply.github.com>
2025-02-16 13:37:28 -0500

    feedback skip message

---
+++ scheduled/feedbackSkip.js
@@ -0,0 +1,30 @@
+const { EmbedBuilder } = require("discord.js");
+const { feedbackChannels, clientId } = require("../config.json");
+
+module.exports = {
+    name: "feedbackSkip",
+    cron: "0 * * * *",
+    enabled: true,
+    async execute() {
+        const { client } = require("../index.js");
+
+        const embed = new EmbedBuilder()
+            .setColor(0x34c3eb)
+            .setTitle("No skipping!")
+            .setDescription("Make sure everyone has gotten feedback before you post. Skipping 3 times = ban from feedback channels.")
+            .setFooter({ text: "This message is automatically sent every hour." });
+
+        for (const channelID of feedbackChannels) {
+            const channel = await client.channels.cache.get(channelID);
+            const messages = await channel.messages.fetch({ limit: 10 });
+            
+            if (await messages.some(
+                (message) => 
+                    message.author.id === clientId
+            ))
+                return;
+    
+            await channel.send({ embeds: [embed] })
+        }
+    },
+};
