d42a67819eb2e8b3ab3b05b59c8a72c4b8c9cfea
sphericle <86196223+sphericle@users.noreply.github.com>
2025-02-24 17:47:00 -0500

    checklevel command

---
+++ commands/others/pingLevel.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require("discord.js")
+
+module.exports = {
+    enabled: true,
+    data: new SlashCommandBuilder()
+        .setName("checklevel")
+        .setDescription("Command to check whether a GD level has been delisted automatically")
+        .addStringOption((option) =>
+            option
+                .setName("id")
+                .setDescription("The level ID to check")
+        ),
+    async execute(interaction) {
+        const lvlId = interaction.options.getString('id');
+        const url = `https://gdbrowser.com/api/level/${lvlId}`
+
+        const req = await fetch(url);
+        if (req.ok) {
+            await interaction.reply(
+                `Found it!\nhttps://gdbrowser.com/${lvlId}`
+            );
+            return;
+        } else {
+            await interaction.reply(
+                `Not found... try **removing any "art" in the level, or other areas with many blocks.** The GD servers automatically detect levels with images added using mods, but they're not very good at it! Blame robtop!`
+            );
+            return;
+        }
+    },
+};
