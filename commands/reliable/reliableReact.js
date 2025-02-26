cc91c66d181c5f63722e2faf9a3b06c70c54e1a4
sphericle <86196223+sphericle@users.noreply.github.com>
2025-02-26 12:28:34 -0500

    check if already reacted

--- commands/reliable/reliableReact.js
+++ commands/reliable/reliableReact.js
@@ -1,6 +1,7 @@
 // bro why are context menus of type app command
 
 const { ContextMenuCommandBuilder, ApplicationCommandType } = require("discord.js");
+const { clientId } = require('../../config.json')
 
 module.exports = {
     enabled: true,
@@ -8,11 +9,19 @@ module.exports = {
         .setName("Add Reactions")
         .setType(ApplicationCommandType.Message),
     async execute(interaction) {
-
         const message = await interaction.channel.messages.fetch(interaction.targetId);
+        const userReactions = message.reactions.cache.some(async (reaction) => {
+            const users = await reaction.users.fetch()
+            return await users.some((user) => user.id === clientId)
+        });
+        
+        if (userReactions) {
+            return interaction.editReply("Already reacted to this message!");
+        }
         await message.react("⬆️")
         await message.react("👍")
         await message.react("⬇️")
+        
         return interaction.editReply("✅")
     },
 };
