0ede58f860fd36ed74157ab568cf8db173b84967
sphericle <86196223+sphericle@users.noreply.github.com>
2025-03-21 19:27:35 -0400

    allow multiple levels with the same name

--- buttons/commitAddLevel.js
+++ buttons/commitAddLevel.js
@@ -24,6 +24,9 @@ module.exports = {
         const level = await db.levelsToPlace.findOne({
             where: { discordid: interaction.message.id },
         });
+
+        let mainFilename = level.filename;
+
         if (!level) {
             await interaction.editReply(
                 ":x: This action is no longer available"
@@ -76,6 +79,26 @@ module.exports = {
             );
         }
 
+         // Check if file already exists
+         try {
+            await octokit.rest.repos.getContent({
+                owner: githubOwner,
+                repo: githubRepo,
+                path: githubDataPath + `/${mainFilename}.json`,
+                branch: githubBranch,
+            });
+            let creator = JSON.parse(level.githubCode).creators[0]
+            creator = creator
+                .normalize("NFD")
+                .replace(/[^a-zA-Z0-9 ]/g, "")
+                .replace(/ /g, "_")
+                .toLowerCase(),
+            mainFilename = level.filename + `_${creator}`
+            
+        } catch (e) {
+            e // lol!
+        }
+
         // filter out all levels that are not dividers
         const noDiv = list.filter((level) => !level.startsWith("_"));
 
@@ -86,225 +109,212 @@ module.exports = {
         const realBelow = list.indexOf(levelBelow);
 
         // insert the level above the real list index
-        list.splice(realBelow, 0, level.filename);
+        list.splice(realBelow, 0, mainFilename);
 
         changelogList.push({
             date: Math.floor(new Date().getTime() / 1000),
             action: "placed",
-            name: level.filename,
+            name: mainFilename,
             to_rank: level.position,
             from_rank: null,
             above: noDiv[level.position] || null,
             below: noDiv[level.position - 2] || null,
         });
+        
+        const changes = [
+            {
+                path: githubDataPath + "/_list.json",
+                content: JSON.stringify(list, null, "\t"),
+            },
+            {
+                path: githubDataPath + `/${mainFilename}.json`,
+                content: level.githubCode,
+            },
+            {
+                path: githubDataPath + "/_changelog.json",
+                content: JSON.stringify(changelogList, null, "\t"),
+            },
+        ];
 
-        // Check if file already exists
+        let commitSha;
         try {
-            await octokit.rest.repos.getContent({
+            // Get the SHA of the latest commit from the branch
+            const { data: refData } = await octokit.git.getRef({
                 owner: githubOwner,
                 repo: githubRepo,
-                path: githubDataPath + `/${level.filename}.json`,
-                branch: githubBranch,
+                ref: `heads/${githubBranch}`,
             });
-            return await interaction.editReply(
-                ":x: The file for this level already exists"
-            );
-        } catch {
-            // File does not exist
-            const changes = [
-                {
-                    path: githubDataPath + "/_list.json",
-                    content: JSON.stringify(list, null, "\t"),
-                },
-                {
-                    path: githubDataPath + `/${level.filename}.json`,
-                    content: level.githubCode,
-                },
-                {
-                    path: githubDataPath + "/_changelog.json",
-                    content: JSON.stringify(changelogList, null, "\t"),
-                },
-            ];
-
-            let commitSha;
-            try {
-                // Get the SHA of the latest commit from the branch
-                const { data: refData } = await octokit.git.getRef({
-                    owner: githubOwner,
-                    repo: githubRepo,
-                    ref: `heads/${githubBranch}`,
-                });
-                commitSha = refData.object.sha;
-            } catch (getRefErr) {
-                logger.info(
-                    `Something went wrong while getting the latest commit SHA: \n${getRefErr}`
-                );
-                return await interaction.editReply(
-                    ":x: Couldn't commit to github, please try again later (getRefError)"
-                );
-            }
-
-            let treeSha;
-            try {
-                // Get the commit using its SHA
-                const { data: commitData } = await octokit.git.getCommit({
-                    owner: githubOwner,
-                    repo: githubRepo,
-                    commit_sha: commitSha,
-                });
-                treeSha = commitData.tree.sha;
-            } catch (getCommitErr) {
-                logger.info(
-                    `Something went wrong while getting the latest commit: \n${getCommitErr}`
-                );
-                return await interaction.editReply(
-                    ":x: Couldn't commit to github, please try again later (getCommitError)"
-                );
-            }
-
-            let newTree;
-            try {
-                // Create a new tree with the changes
-                newTree = await octokit.git.createTree({
-                    owner: githubOwner,
-                    repo: githubRepo,
-                    base_tree: treeSha,
-                    tree: changes.map((change) => ({
-                        path: change.path,
-                        mode: "100644",
-                        type: "blob",
-                        content: change.content,
-                    })),
-                });
-            } catch (createTreeErr) {
-                logger.info(
-                    `Something went wrong while creating a new tree: \n${createTreeErr}`
-                );
-                return await interaction.editReply(
-                    ":x: Couldn't commit to github, please try again later (createTreeError)"
-                );
-            }
-
-            let newCommit;
-            try {
-                // Create a new commit with this tree
-                newCommit = await octokit.git.createCommit({
-                    owner: githubOwner,
-                    repo: githubRepo,
-                    message: `Placed ${level.filename} at ${level.position} (${interaction.user.tag})`,
-                    tree: newTree.data.sha,
-                    parents: [commitSha],
-                });
-            } catch (createCommitErr) {
-                logger.info(
-                    `Something went wrong while creating a new commit: \n${createCommitErr}`
-                );
-                return await interaction.editReply(
-                    ":x: Couldn't commit to github, please try again later (createCommitError)"
-                );
-            }
-
-            try {
-                // Update the branch to point to the new commit
-                await octokit.git.updateRef({
-                    owner: githubOwner,
-                    repo: githubRepo,
-                    ref: `heads/${githubBranch}`,
-                    sha: newCommit.data.sha,
-                });
-            } catch (updateRefErr) {
-                logger.info(
-                    `Something went wrong while updating the branch reference: \n${updateRefErr}`
-                );
-                return await interaction.editReply(
-                    ":x: Couldn't commit to github, please try again later (updateRefError)"
-                );
-            }
-
-            try {
-                // this was changed to -1 because the lvl to place is
-                // never added to the noDiv array
-                const above = noDiv[level.position]
-                    ? await cache.levels.findOne({
-                          where: { filename: noDiv[level.position - 1] },
-                      })
-                    : null;
-                const below = noDiv[level.position - 2]
-                    ? await cache.levels.findOne({
-                          where: { filename: noDiv[level.position - 2] },
-                      })
-                    : null;
-
-                if (enableChangelogMessage) {
-                    const levelData = JSON.parse(level.githubCode);
-                    await db.changelog.create({
-                        levelname: levelData.name,
-                        old_position: null,
-                        new_position: level.position,
-                        level_above: above?.name || null,
-                        level_below: below?.name || null,
-                        action: "placed",
-                    });
-
-                    let message = `${levelData.name} has been placed at #${level.position}, `;
-                    if (above) message += `above ${above?.name}`;
-                    if (above && below) message += ` and `;
-                    if (below) message += `below ${below?.name}`;
-                    message += ".";
-
-                    // Create embed to send in public channel
-                    const publicEmbed = new EmbedBuilder()
-                        .setColor(0x8fce00)
-                        .setTitle(`:white_check_mark: ${levelData.name}`) // TODO: maybe make this a random funny message
-                        .setDescription(message)
-                        .setTimestamp();
-
-                    const guild = await interaction.client.guilds.fetch(
-                        guildId
-                    );
-                    const staffGuild = enableSeparateStaffServer
-                        ? await interaction.client.guilds.fetch(staffGuildId)
-                        : guild;
-
-                    const announcementMsg = await staffGuild.channels.cache
-                        .get(changelogID)
-                        .send({
-                            embeds: [publicEmbed],
-                        });
-                    await announcementMsg.crosspost();
-                }
-            } catch (changelogErr) {
-                logger.info(
-                    `An error occured while creating a changelog entry:\n${changelogErr}`
-                );
-                return await interaction.editReply(
-                    `:white_check_mark: Successfully created file: **${level.filename}.json** (${newCommit.data.html_url}), but an error occured while creating a changelog entry`
-                );
-            }
-
+            commitSha = refData.object.sha;
+        } catch (getRefErr) {
             logger.info(
-                `${interaction.user.tag} (${interaction.user.id}) placed ${level.filename} at ${level.position}`
+                `Something went wrong while getting the latest commit SHA: \n${getRefErr}`
             );
-            try {
-                logger.info(
-                    `Successfully created commit on ${githubBranch}: ${newCommit.data.sha}`
-                );
-                db.levelsToPlace.destroy({
-                    where: { discordid: level.discordid },
-                });
-                cache.levels.create({
-                    name: JSON.parse(level.githubCode).name,
-                    filename: level.filename,
-                    position: level.position,
-                });
-            } catch (cleanupErr) {
-                logger.info(
-                    `Successfully created commit on ${githubBranch}: ${newCommit.data.sha}, but an error occured while cleaning up:\n${cleanupErr}`
-                );
-            }
             return await interaction.editReply(
-                `:white_check_mark: Successfully created file: **${level.filename}.json** (${newCommit.data.html_url})`
+                ":x: Couldn't commit to github, please try again later (getRefError)"
             );
         }
+
+        let treeSha;
+        try {
+            // Get the commit using its SHA
+            const { data: commitData } = await octokit.git.getCommit({
+                owner: githubOwner,
+                repo: githubRepo,
+                commit_sha: commitSha,
+            });
+            treeSha = commitData.tree.sha;
+        } catch (getCommitErr) {
+            logger.info(
+                `Something went wrong while getting the latest commit: \n${getCommitErr}`
+            );
+            return await interaction.editReply(
+                ":x: Couldn't commit to github, please try again later (getCommitError)"
+            );
+        }
+
+        let newTree;
+        try {
+            // Create a new tree with the changes
+            newTree = await octokit.git.createTree({
+                owner: githubOwner,
+                repo: githubRepo,
+                base_tree: treeSha,
+                tree: changes.map((change) => ({
+                    path: change.path,
+                    mode: "100644",
+                    type: "blob",
+                    content: change.content,
+                })),
+            });
+        } catch (createTreeErr) {
+            logger.info(
+                `Something went wrong while creating a new tree: \n${createTreeErr}`
+            );
+            return await interaction.editReply(
+                ":x: Couldn't commit to github, please try again later (createTreeError)"
+            );
+        }
+
+        let newCommit;
+        try {
+            // Create a new commit with this tree
+            newCommit = await octokit.git.createCommit({
+                owner: githubOwner,
+                repo: githubRepo,
+                message: `Placed ${mainFilename} at ${level.position} (${interaction.user.tag})`,
+                tree: newTree.data.sha,
+                parents: [commitSha],
+            });
+        } catch (createCommitErr) {
+            logger.info(
+                `Something went wrong while creating a new commit: \n${createCommitErr}`
+            );
+            return await interaction.editReply(
+                ":x: Couldn't commit to github, please try again later (createCommitError)"
+            );
+        }
+
+        try {
+            // Update the branch to point to the new commit
+            await octokit.git.updateRef({
+                owner: githubOwner,
+                repo: githubRepo,
+                ref: `heads/${githubBranch}`,
+                sha: newCommit.data.sha,
+            });
+        } catch (updateRefErr) {
+            logger.info(
+                `Something went wrong while updating the branch reference: \n${updateRefErr}`
+            );
+            return await interaction.editReply(
+                ":x: Couldn't commit to github, please try again later (updateRefError)"
+            );
+        }
+
+        try {
+            // this was changed to -1 because the lvl to place is
+            // never added to the noDiv array
+            const above = noDiv[level.position]
+                ? await cache.levels.findOne({
+                        where: { filename: noDiv[level.position - 1] },
+                    })
+                : null;
+            const below = noDiv[level.position - 2]
+                ? await cache.levels.findOne({
+                        where: { filename: noDiv[level.position - 2] },
+                    })
+                : null;
+
+            if (enableChangelogMessage) {
+                const levelData = JSON.parse(level.githubCode);
+                await db.changelog.create({
+                    levelname: levelData.name,
+                    old_position: null,
+                    new_position: level.position,
+                    level_above: above?.name || null,
+                    level_below: below?.name || null,
+                    action: "placed",
+                });
+
+                let message = `${levelData.name} has been placed at #${level.position}, `;
+                if (above) message += `above ${above?.name}`;
+                if (above && below) message += ` and `;
+                if (below) message += `below ${below?.name}`;
+                message += ".";
+
+                // Create embed to send in public channel
+                const publicEmbed = new EmbedBuilder()
+                    .setColor(0x8fce00)
+                    .setTitle(`:white_check_mark: ${levelData.name}`) // TODO: maybe make this a random funny message
+                    .setDescription(message)
+                    .setTimestamp();
+
+                const guild = await interaction.client.guilds.fetch(
+                    guildId
+                );
+                const staffGuild = enableSeparateStaffServer
+                    ? await interaction.client.guilds.fetch(staffGuildId)
+                    : guild;
+
+                const announcementMsg = await staffGuild.channels.cache
+                    .get(changelogID)
+                    .send({
+                        embeds: [publicEmbed],
+                    });
+                await announcementMsg.crosspost();
+            }
+        } catch (changelogErr) {
+            logger.info(
+                `An error occured while creating a changelog entry:\n${changelogErr}`
+            );
+            return await interaction.editReply(
+                `:white_check_mark: Successfully created file: **${mainFilename}.json** (${newCommit.data.html_url}), but an error occured while creating a changelog entry`
+            );
+        }
+
+        logger.info(
+            `${interaction.user.tag} (${interaction.user.id}) placed ${mainFilename} at ${level.position}`
+        );
+        try {
+            logger.info(
+                `Successfully created commit on ${githubBranch}: ${newCommit.data.sha}`
+            );
+            db.levelsToPlace.destroy({
+                where: { discordid: level.discordid },
+            });
+            cache.levels.create({
+                name: JSON.parse(level.githubCode).name,
+                filename: mainFilename,
+                position: level.position,
+            });
+        } catch (cleanupErr) {
+            logger.info(
+                `Successfully created commit on ${githubBranch}: ${newCommit.data.sha}, but an error occured while cleaning up:\n${cleanupErr}`
+            );
+        }
+        return await interaction.editReply(
+            `:white_check_mark: Successfully created file: **${mainFilename}.json** (${newCommit.data.html_url})`
+        );
+        
     },
 };
