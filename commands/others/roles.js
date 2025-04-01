bd8556c40aff7c8da46ded746ba5340e382d41aa
sphericle <86196223+sphericle@users.noreply.github.com>
2025-04-01 18:05:58 -0400

    finish up this ting

--- commands/others/roles.js
+++ commands/others/roles.js
@@ -1,61 +1,149 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, PermissionsBitField } = require("discord.js");
+const { adminRole } = require("../../config.json")
+const logger = require('log4js').getLogger()
 
 module.exports = {
     enabled: true,
     data: new SlashCommandBuilder()
-        .setName("editrole")
-        .setDescription("Manage custom role")
-        .addStringOption((option) =>
-            option
-                .setName("color")
-                .setDescription("The new hex code to use for the color")
+        .setName("customrole")
+        .setDescription(
+            "Manage custom roles"
         )
-        .addStringOption((option) =>
-            option.setName("name").setDescription("The new role name")
+        .addSubcommand((subcommand) => 
+            subcommand
+                .setName("create")
+                .setDescription("Create your custom role!")
+                .addStringOption((option) => 
+                    option
+                        .setName("color")
+                        .setDescription("The new hex code to use for the color")
+                )
+                .addStringOption((option) => 
+                    option
+                        .setName("name")
+                        .setDescription("The new role name")
+                )
+                .addStringOption((option) => 
+                    option
+                        .setName("icon")
+                        .setDescription("A link to the icon for your role")
+                )
         )
-        .addStringOption((option) =>
-            option.setName("icon").setDescription("A link to your role's icon")
+        .addSubcommand((subcommand) => 
+            subcommand
+                .setName("edit")
+                .setDescription("Create your custom role!")
+                .addStringOption((option) => 
+                    option
+                        .setName("color")
+                        .setDescription("The new hex code to use for the color")
+                )
+                .addStringOption((option) => 
+                    option
+                        .setName("name")
+                        .setDescription("The new role name")
+                )
+                .addStringOption((option) => 
+                    option
+                        .setName("icon")
+                        .setDescription("A link to your role's icon")
+                )
         ),
     async execute(interaction) {
-        await interaction.deferReply({ ephemeral: true });
+        await interaction.deferReply({ ephemeral: true })
+        const { db } = require("../../index.js")
+        const subcommand = await interaction.options.getSubcommand()
+        if (subcommand === "edit") {
+            let color = interaction.options.getString("color")
+            const name = interaction.options.getString("name")
+            const icon = interaction.options.getString("icon")
 
-        let color = interaction.options.getString("color");
-        const name = interaction.options.getString("name");
-        const icon = interaction.options.getString("icon");
+            if (color && (color.startsWith("#"))) {
+                color = color.substring(1)
+            }
 
-        if (color && color.startsWith("#")) {
-            color = color.substring(1);
+            let dbRole;
+            try {
+                dbRole = await db.adminRoles.findOne({
+                    where: {
+                        adminid: interaction.user.id
+                    }
+                })
+            } catch (e) {
+                logger.error(`Error finding role in database! ${e}`)
+                return await interaction.editReply("Error editing role: failed to get role from database!")
+                
+            }
+
+            if (!dbRole) {
+                return interaction.editReply("Error editing role: not found in db!")
+            }
+
+            let role;
+            try {
+                role = await interaction.member.roles.cache.get(dbRole.roleId)
+                if (!role) {
+                    return interaction.editReply(":x: Could not find your custom role, ensure it isn't deleted and you still have it")
+                }
+                await role.edit({
+                    ...(color ? { color } : {}),
+                    ...(name ? { name } : {}),
+                    ...(icon ? { icon } : {})
+                })
+            } catch (e) {
+                return interaction.editReply(`Error editing role: ${e}`)
+            }
+
+            return interaction.editReply("Edited your role!")
+        } else if (subcommand === "create") {
+            let color = interaction.options.getString("color")
+            const name = interaction.options.getString("name")
+            const icon = interaction.options.getString("icon")
+
+            if (color && (color.startsWith("#"))) {
+                color = color.substring(1)
+            }
+
+            const { db } = require("../../index.js")
+            
+            let listAdminRole
+            try {
+                listAdminRole = await interaction.guild.roles.cache.get(adminRole);
+                if (!listAdminRole) {
+                    logger.warn(`No list admin role found! ID: ${adminRole}`)
+                }
+            } catch (e) {
+                logger.error(`Error fetching list admin role (${adminRole}): ${e}`)
+                return await interaction.editReply("Error creating custom role: could not find admin role!")
+            }
+
+            const role = await interaction.guild.roles.create({
+                name: name || interaction.user.displayName,
+                color: "Default",
+                hoist: false,
+                mentionable: false,
+                permissions: PermissionsBitField.Default,
+                position: listAdminRole.position + 1,
+                reason: `${interaction.user.username} created their custom role`,
+                ...(icon ? { icon } : {})
+            })
+
+            try {
+                await db.adminRoles.create({
+                    adminid: interaction.user.id,
+                    roleId: role.id
+                })
+            } catch (e) {
+                logger.error(`Error adding ${interaction.user.username}'s custom role to the database: ${e}`)
+            }
+            try {
+                await interaction.member.roles.cache.add(role.id)
+            } catch (e) {
+                logger.error(`Failed to add ${interaction.user.id}'s custom role: ${e}`)
+                return await interaction.editReply("Created your role, but there was a problem trying to add it to you :/")
+            }
+
+            return interaction.editReply(`Created your role! <@&${role.id}>`)
         }
-
-        const { db } = require("../../index.js");
-        const dbRole = await db.adminRoles.findOne({
-            where: {
-                adminid: interaction.user.id,
-            },
-        });
-
-        if (!dbRole) {
-            return interaction.editReply(
-                "Error editing role: not found in db!"
-            );
-        }
-
-        const role = await interaction.member.roles.cache.get(dbRole.roleId);
-        if (!role) {
-            return interaction.editReply(
-                ":x: Could not find you custom role, ensure it isn't deleted and you still have it"
-            );
-        }
-        try {
-            await role.edit({
-                ...(color ? { color } : {}),
-                ...(name ? { name } : {}),
-                ...(icon ? { icon } : {}),
-            });
-        } catch (e) {
-            return interaction.editReply(`Error editing role: ${e}`);
-        }
-
-        return interaction.editReply("Edited your role!");
     },
 };
--- events/manageCustomRoles.js
+++ events/manageCustomRoles.js
@@ -1,6 +1,5 @@
 const { Events, PermissionsBitField } = require("discord.js");
-const logger = require("log4js").getLogger();
-const { adminRole } = require("../config.json");
+const { adminRole } = require("../config.json")
 
 module.exports = {
     name: Events.GuildMemberUpdate,
@@ -8,13 +7,10 @@ module.exports = {
         const { db } = require("../index.js");
 
         const listAdminRole = await oldMember.guild.roles.cache.get(adminRole);
-        const oldMemberHasRole = await oldMember.roles.cache.has(
-            listAdminRole.id
-        );
-        const newMemberHasRole = await newMember.roles.cache.has(
-            listAdminRole.id
-        );
-
+        const oldMemberHasRole = await oldMember.roles.cache.has(listAdminRole.id);
+        const newMemberHasRole = await newMember.roles.cache.has(listAdminRole.id);
+        
+        // if this user just got admin
         if (!oldMemberHasRole && newMemberHasRole) {
             const newRole = await oldMember.guild.roles.create({
                 name: oldMember.user.displayName,
@@ -22,11 +18,9 @@ module.exports = {
                 hoist: false,
                 mentionable: false,
                 permissions: PermissionsBitField.Default,
-                position: listAdminRole.position - 1,
-                reason: `${oldMember.user.username} was promoted to admin`,
+                position: listAdminRole.position + 1,
+                reason: `${oldMember.user.username} was promoted to admin`
             });
-
-            // if this user was promoted to admin
             await db.adminRoles.create({
                 roleId: newRole.id,
                 adminId: oldMember.id,
@@ -34,23 +28,6 @@ module.exports = {
 
             await oldMember.roles.add(newRole);
             return;
-        } else if (oldMemberHasRole && !newMemberHasRole) {
-            // if this user was demoted from admin
-            const dbRole = await db.adminRoles.findOne({
-                where: {
-                    adminId: oldMember.id,
-                },
-            });
-            await dbRole.destroy();
-            if (!dbRole) {
-                logger.error(
-                    `Error deleting role from ${oldMember.user.username}! Role not found in DB.`
-                );
-                return;
-            }
-
-            await oldMember.guild.roles.delete(dbRole.roleId);
-            logger.log(`Deleted ${oldMember.user.username}'s custom role!`);
         }
     },
 };
